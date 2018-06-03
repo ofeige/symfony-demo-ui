5c6cc03e7e6bcbb232039b8d532af539f4a5f340
Oliver Feige <oliverfeige@mbpr.local>
2018-06-03 23:20:36 +0200

    base commit

---
+++ src/Controller/EasyAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class EasyAdmin extends BaseAdminController
+{
+    protected $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function persistEntity($entity)
+    {
+        $this->updatePassword($entity);
+        parent::persistEntity($entity);
+    }
+
+    private function updatePassword($entity)
+    {
+        if (is_subclass_of($entity, UserInterface::class) && method_exists($entity, 'getPlainPassword')) {
+            if (($plainPassword = $entity->getPlainPassword()) == '') {
+                return $entity;
+            }
+            $entity->setPassword($this->passwordEncoder->encodePassword($entity, $entity->getPlainPassword()));
+        }
+
+        return $entity;
+    }
+
+    public function updateEntity($entity)
+    {
+        $this->updatePassword($entity);
+        parent::updateEntity($entity);
+    }
+}
