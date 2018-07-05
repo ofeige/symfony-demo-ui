7db3d0fbace42a8ae0d88504245150f52469936b
Nicolas Hohm <nicolas.hohm@check24.de>
2018-07-05 09:42:06 +0200

    Add ekino newrelic bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
+    Ekino\NewRelicBundle\EkinoNewRelicBundle::class => ['all' => true],
 ];
