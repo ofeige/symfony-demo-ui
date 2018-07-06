a9e92c6c420420f555f247669205f0c0ea07ffef
Michael Wolf <michael.wolf@check24.de>
2018-07-06 11:17:54 +0200

    Fixed UserProfileType

--- src/Controller/Profile.php
+++ src/Controller/Profile.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\UserProfile;
-use App\Form\UserProfileType;
+use App\Form\Type\UserProfileType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
