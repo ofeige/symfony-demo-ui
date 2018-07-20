dd0afb1c766b6392752357271229016b58121993
Michael Wolf <michael.wolf@check24.de>
2018-07-20 10:09:42 +0200

    Fixing Insight Violations

--- src/ApiClient/ApiClient.php
+++ src/ApiClient/ApiClient.php
@@ -3,17 +3,14 @@
 namespace App\ApiClient;
 
 use Api\Dto\ItemV1;
-use Api\Dto\ItemV2;
 use Api\Dto\UserV1;
 use Api\Dto\UserV2;
 use Api\Dto\UserV3;
-use Api\Dto\UserV4;
 use GuzzleHttp\Client;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
